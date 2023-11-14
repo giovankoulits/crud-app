ff62fe40c303d1dced07cb32d3f1d887372c3c32
chupacambra <jovankoul@hotmail.com>
2023-11-14 19:56:48 +0200

    Added simple express functionality

--- server.js
+++ server.js
@@ -5,40 +5,43 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 const { lookup } = require("mime-types");
-const port = process.env.PORT;
-const server = http.createServer(function (req, res) {
+const port = process.env.DEV_PORT;
+const express = require("express");
+const books = require("./books");
+//
+const app = express();
+
+app.get("/books/:id/henlo/:cat", (req, res) => {
+  const bookIndex = parseInt(req.params.id);
+  console.log(req.params);
+
+  if (bookIndex <= books.length) {
+    res.send(books[bookIndex]);
+  } else {
+    res.send("henlo");
+  }
+});
+
+/* app.use("/", (req, res) => {
+  num += 1;
+  console.log(num);
+  res.send("hello world");
+}); */
+
+/* const server = http.createServer(function (req, res) {
   //
 
   const filter = /^\/+/g;
   const myUrl = url.parse(req.url);
   let parsedUrl = myUrl.pathname.replace(filter, "");
-  let file = path.join(__dirname, "public", parsedUrl);
+
+  console.log(myUrl.query);
 
   if (parsedUrl === "") {
     file = "./public/index.html";
   }
   res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      console.log(`File not found ${file}`);
-      res.writeHead(404);
-      fs.readFile("./public/error.html", (err, data) => res.end(data));
-    } else {
-      const mime = lookup(parsedUrl);
-      res.writeHead(200, { "Content-type": mime });
-      switch (parsedUrl) {
-        case "index.html":
-          res.writeHead(200, { "Content-type": "text/html" });
-          break;
-        case "about.html":
-          res.writeHead(200, { "Content-type": "text/html" });
-          break;
-        default:
-          break;
-      }
-      res.end(data);
-    }
-  });
-});
+  res.end();
+}); */
 
-server.listen(3000, () => console.log(`Listening on port ${port}`));
+app.listen(3000, () => console.log(`Listening on port ${port}`));
