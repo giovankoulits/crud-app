86a4ed7aa8a48d73ce92190848213adcf661e6b6
chupacambra <jovankoul@hotmail.com>
2023-11-28 22:30:59 +0200

    Added mongoDB and db functionality

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+
+// view engine
+app.set("view engine", "ejs");
+app.use(express.json());
+
+// database connection
+const dbURI =
+  "mongodb+srv://jovankoul:cabul1453@cluster0.okbvxm5.mongodb.net/node-auth";
+
+mongoose
+  .connect(dbURI)
+  .then((result) =>
+    app.listen(3000, () => console.log("listening on port 3000"))
+  )
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
