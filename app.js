287a92342bf09fcc8d4b0fc031429d8387ebd544
chupacambra <jovankoul@hotmail.com>
2023-11-29 17:22:25 +0200

    Added signup and login pages

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ mongoose
     app.listen(3000, () => console.log("listening on port 3000"))
   )
   .catch((err) => console.log(err));
-
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
+const { compare } = require("bcrypt");
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
+const saltRounds = 13;
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -14,6 +17,17 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please enter a password"],
     minlength: [6, "Minimum password length is 6 characters"],
   },
+});
+
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, saltRounds);
+
+  next();
+});
+
+userSchema.post("save", function (doc, next) {
+  console.log("new user was created & saved");
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
